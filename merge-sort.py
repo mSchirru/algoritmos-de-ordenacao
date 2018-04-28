ca9bdb86b8a549722d7112f8247a552a7d63aab9
Mikael Schirru <2014.schirru@gmail.com>
2018-04-28 15:47:50 -0300

    Update merge-sort.py

--- merge-sort.py
+++ merge-sort.py
@@ -1,1 +1,39 @@
 
+def merge_sort(lista):
+  
+  if len(lista) > 1:
+    mid = len(lista) // 2
+    
+    lado_esquerdo = lista[:mid]
+    lado_direito = lista[mid:]
+    
+    merge_sort(lado_esquerdo)
+    merge_sort(lado_direito)
+  
+    i = 0 
+    j = 0
+    k = 0
+    
+    while i < len(lado_esquerdo) and j < len(lado_direito):
+        if lado_esquerdo[i] < lado_direito[j]:
+          lista[k] = lado_esquerdo[i]
+          i += 1
+        else: 
+          lista[k] = lado_direito[j]
+          j += 1
+        k += 1
+    
+    while i < len(lado_esquerdo):
+      lista[k] = lado_esquerdo[i]
+      i += 1
+      k += 1
+      
+    while j < len(lado_direito):
+      lista[k] = lado_direito[j]
+      j += 1
+      k += 1
+  print(lista)
+
+lista = [10,5,30,1,2,444,64,80,5,1,444,65,64]
+
+print(merge_sort(lista))
