a467cdb74d5db050a31fb3076b71c9fcb7dbc4f3
Mikael Schirru <2014.schirru@gmail.com>
2018-04-28 15:52:41 -0300

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,1 +1,16 @@
 
+def bubble_sort(lista):
+  elementos =  len(lista)-1
+  flag = False
+  While not flag:
+    flag = True
+    for i in range(elementos):
+      if lista[i] > lista[i+1]:
+        lista[i], lista[i+1] = lista[i+1], lista[i]
+        flag = False
+        print(lista)
+        
+  return lista
+
+lista = [4,6,2,1,6,76,543,234,645,9,1,0]
+print(bubble_sort(lista))
